fd716e43b42d1e6f1c0841c668c8da4bd5b5bca8
Chris Elchik <christopherwe6@gmail.com>
2025-06-09 16:21:06 -0400

    Installed pendo

--- public/js/initPendo.js
+++ public/js/initPendo.js
@@ -1,7 +1,19 @@
+
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+
+        pendo.initialize({
+            visitor: {
+                id:              5,
+                email:        'test@test.com',
+            },
+
+            account: {
+                id:              6,
+            }
+        });
 })('f9f576c8-0669-4d72-45d3-f6da4208acc5');
